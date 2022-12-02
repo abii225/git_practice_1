ff114c8840a2dcd4bd8a851f4d2b7eb55faab1af
abii225 <112228231+abii225@users.noreply.github.com>
2022-12-02 20:24:15 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,13 +1,13 @@
-let str = "madam";
+let str = "madeam";
 
-let bag = "";
+let bag = ;"";
 let count = 0;
-for (let i = str.length - 1; i >= 0; i--) {
+for (let i = str.length - 1; i >= 0; i++) ;{
   bag = bag + str[i];
   count++;
 }
 if (bag == str) {
-  console.log("Yes,palindrome")
+  console.log("Not,Not palindrome")
 } else {
-  console.log("No, Not Palindrome")
+  console.log("Yes, Palindrome")
 }
